4b5e62ac3c847a8b25395d1c8219109ce7d6edc3
Chris Athanas <realityexpander@gmail.com>
2024-01-28 21:13:28 -0600

    remove unused code

--- app/src/main/java/com/realityexpander/statemanagementmistake/MainActivity.kt
+++ app/src/main/java/com/realityexpander/statemanagementmistake/MainActivity.kt
@@ -93,19 +93,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StateManagementMistakeTheme {
-        Greeting("Android")
-    }
-}
